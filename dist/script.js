a47e21a1f68e6e88b4418886329912199bad529c
fhrrrzy <fahrurrozy4214@gmail.com>
2023-01-26 15:57:26 +0700

    dekstop view

--- dist/script.js
+++ dist/script.js
@@ -8,7 +8,7 @@ let sidenav = $("#mobile-left-nav")
 let wrapper = $(".wrapper")
 $("#mobile-nav-open").click(()=>{
     if(!isOpen){
-        sidenav.removeClass("-translate-x-[35rem] ")
+        sidenav.removeClass("-translate-x-[30rem] ")
         sidenav.addClass("z-[999999] translate-x-0")
         wrapper.addClass("blur-sm")
         isOpen = true
@@ -19,25 +19,30 @@ $("#mobile-nav-close, #dismiss-side").click(()=>{
     if(isOpen){
         sidenav.removeClass("z-[999999] translate-x-0")
         wrapper.removeClass("blur-sm")
-        sidenav.addClass("-translate-x-[35rem]")
+        sidenav.addClass("-translate-x-[30rem]")
         isOpen = false
     }
 })
 
 let topbar = $("#topbar")
 var prevScrollpos = window.pageYOffset;
+// Create a media condition that targets viewports at least 768px wide
+const mediaQuery = window.matchMedia('(max-width: 648px)')
 
 const navScroll =  () => {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        if(topbar.hasClass("-translate-y-12")){
-            $("#topbar").removeClass("-translate-y-12")
+    if (mediaQuery.matches) {
+        var currentScrollPos = window.pageYOffset;
+        if (prevScrollpos > currentScrollPos) {
+            if(topbar.hasClass("-translate-y-12")){
+                $("#topbar").removeClass("-translate-y-12")
+            }
+        } else {
+            if(!topbar.hasClass("-translate-y-12")){
+                $("#topbar").addClass("-translate-y-12")
+            }
         }
-    } else {
-        if(!topbar.hasClass("-translate-y-12")){
-            $("#topbar").addClass("-translate-y-12")
-        }
+        prevScrollpos = currentScrollPos;
     }
-    prevScrollpos = currentScrollPos;
 }
+
 window.onscroll = navScroll
