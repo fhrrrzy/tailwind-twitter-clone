045020202a06321430e5824292f5af51d1d011f3
fhrrzy <fahrurrozy4214@gmail.com>
2023-02-24 02:10:04 +0700

    comment for jsdocs test

--- dist/script.js
+++ dist/script.js
@@ -1,12 +1,24 @@
+/**
+ * this add height for every line on write tweet textarea
+ * @param {*} element 
+ */
 function textAreaAdjust(element) {
     element.style.height = "40px";
     element.style.height = (25+element.scrollHeight)+"px";
 }
 
+/**
+ * mobile nav flag
+ */
 let isOpen = false
+
 let sidenav = $("#mobile-left-nav")
 let wrapper = $(".wrapper")
-$("#mobile-nav-open").click(()=>{
+
+/**
+ * event that handle mobile nav on to show 
+ */
+$("#mobile-nav-open").on("click", ()=>{
     if(!isOpen){
         sidenav.removeClass("-translate-x-[30rem] ")
         sidenav.addClass("z-[999999] translate-x-0")
@@ -15,7 +27,10 @@ $("#mobile-nav-open").click(()=>{
     }
 })
 
-$("#mobile-nav-close, #dismiss-side").click(()=>{
+/**
+ * event that handle mobile nav on to dismiss 
+ */
+$("#mobile-nav-close, #dismiss-side").on("click", ()=>{
     if(isOpen){
         sidenav.removeClass("z-[999999] translate-x-0")
         wrapper.removeClass("blur-sm")
@@ -24,6 +39,9 @@ $("#mobile-nav-close, #dismiss-side").click(()=>{
     }
 })
 
+/**
+ * mobile top nav slightly translated to top on scroll
+ */
 let topbar = $("#topbar")
 var prevScrollpos = window.pageYOffset;
 // Create a media condition that targets viewports at least 768px wide
